71aff3d8d1701fcb839553af619237a65e14fd5f
horowolf <13122358+horowolf@users.noreply.github.com>
2024-08-02 00:25:17 +0900

    Initial Commit

---
+++ Bit/BitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitApp.swift
+//  Bit
+//
+//  Created by horo on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
